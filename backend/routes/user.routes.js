df76c68d75da8390c32f8794a71f702fa9731187
nishapoudel <nishapoudel400@gmail.com>
2025-06-19 21:05:55 -0400

    SCRUM-38: Implement middleware to check if user is logged in and protect logout route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -6,16 +6,18 @@ import {
   getUserById,
 } from "../controllers/user.controllers.js";
 import { refreshAccessTokenMiddleware } from "../middlewares/refreshToken.middlewares.js";
+import { isLoggedIn } from "../middlewares/isLoggedIn.js"; 
 
 const router = Router();
 
-//unprotected routes
+//  Unprotected routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/:id", getUserById);
 
-//protected routes
-router.post("/logout", logOutUser);
+// Protected routes
+router.post("/logout", isLoggedIn, logOutUser);
+
 router.get("/refresh-token", refreshAccessTokenMiddleware, (req, res) => {
   return res.status(200).json({
     accessToken: req.newAccessToken,
