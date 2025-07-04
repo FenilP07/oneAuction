a832547b58d3f19c3d9f7f9a7707e3586f696773
PatelRaj1701 <1712001rajpatel@gmail.com>
2025-07-03 21:16:11 -0400

    about us page and some other enhancement with some pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,17 +10,18 @@ import "./index.css";
 import RegistrationForm from "./pages/Register/RegistrationForm";
 import User from "./pages/User/user.jsx";
 import AddCategory from "./pages/Admin/Categories/AddCategory.jsx";
-import AuctioneerItemPage from "./pages/AuctioneerItemPage/AuctioneerItemPage.jsx";
+import AuctioneerItemPage from "./pages/ItemListingPage/AuctioneerItemPage.jsx";
 import Home from "./pages/Home/Home.jsx";
 import AdminDashboard from "./pages/Admin/AdminDasboard.jsx";
 import CategoryList from "./pages/Admin/Categories/CategoryList.jsx";
+import AboutUs from "./pages/AboutUs/AboutUs.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route index element={<Login />} />
+          <Route index element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<RegistrationForm />} />
           <Route path="forgot-password" element={<ForgotPassword />} />
@@ -29,8 +30,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <Route path="auctioneerItemPage" element={<AuctioneerItemPage />} />
           <Route path="home" element={<Home />} />
           <Route path="addcategory" element={<AddCategory />} />
+          <Route path="categorieslist" element={<CategoryList />} />
           <Route path="admin" element={<AdminDashboard />} />
           <Route path="dashboard" element={<Dashboard />} />
+          <Route path="aboutUs" element={<AboutUs />} />
         </Route>
       </Routes>
     </BrowserRouter>
