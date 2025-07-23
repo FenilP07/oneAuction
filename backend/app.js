c9dfcfe8bac0763e7a1c0f4e6f09c77ccba16811
FenilPatel <your-email@example.com>
2025-07-22 21:58:35 -0400

    backend cors fix

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,8 @@ const allowedOrigins = [
   process.env.CLIENT_URL || "http://localhost:5173",
   "http://localhost:3000",
   "https://4e8b4fafd5b6.ngrok-free.app",
-  "https://oneauctionbackend.onrender.com", 
+  "https://oneauctionbackend.onrender.com",
+   "https://oneauctionfrontend.vercel.app", 
   "*"
 ];
 
